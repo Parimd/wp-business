ec9b1b35ab7ca0a440341f78f3ebc578c14bf52f
James Curwood <jac96@cornell.edu>
2019-02-06 19:06:31 -0500

    Create wp-business.php

---
+++ wp-business.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP-Business
+ * Plugin URI: http://flyingsound.net/wp-business
+ * Description: Business tools for WordPress.
+ * Version: 0.1
+ * Author: James Alexander Curwood
+ * Author URI: http://instagram.com/phoenixsoap
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ ?>
